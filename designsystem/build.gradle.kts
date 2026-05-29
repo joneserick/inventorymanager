475ec1ec0fbd178f7e19a11910e52ceb0231a53a
Erick Jones <erick.silva3@ab-inbev.com>
2026-05-29 19:40:55 -0300

    feat(core/designsystem): create designsystem and base models

--- designsystem/build.gradle.kts
+++ designsystem/build.gradle.kts
@@ -1,21 +1,18 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
 
 android {
-    namespace = "com.stefick.inventorymanager"
+    namespace = "com.stefick.designsystem"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.stefick.inventorymanager"
         minSdk = 27
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -33,29 +30,23 @@ android {
     }
     kotlinOptions {
         jvmTarget = "17"
-
     }
+
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "2.0.0"
-    }
+
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    debugImplementation(libs.androidx.compose.ui.tooling)
 }
---
+++ designsystem/src/main/java/com/stefick/designsystem/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.stefick.designsystem.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryBlue = Color(0xFF2563EB) // Azul tecnológico para ações
+val SecondaryBlue = Color(0xFF1E40AF)
+val BackgroundGray = Color(0xFFF8FAFC) // Off-white para não cansar a vista
+val SurfaceWhite = Color(0xFFFFFFFF)
+val TextDark = Color(0xFF1E293B) // Azul marinho quase preto (melhor que preto puro)
+val TextLight = Color(0xFF64748B)
+val ErrorRed = Color(0xFFDC2626) // Alertas de estoque
