a30e27e511d5cf59b50645827f33d5e6391acf9f
Erick Jones <erick.silva3@ab-inbev.com>
2026-06-05 09:55:31 -0300

    introduce UDF principle to inventory workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,14 +49,21 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
     implementation(libs.androidx.hilt.navigation.compose)
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
+
+    implementation(libs.androidx.paging.runtime)
+    implementation(libs.androidx.paging.compose)
+    implementation(libs.androidx.paging.common)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/stefick/inventorymanager/features/inventory/InventoryContract.kt
@@ -0,0 +1,19 @@
+package com.stefick.inventorymanager.features.inventory
+
+import androidx.paging.PagingData
+import com.stefick.core.model.InventoryItem
+import kotlinx.coroutines.flow.Flow
+
+sealed class InventoryUiState {
+    object Loading : InventoryUiState()
+    data class Error(val message: String) : InventoryUiState()
+    data class Loaded(
+        val items: Flow<PagingData<InventoryItem>>
+    ) : InventoryUiState()
+}
+
+
+sealed class InventoryEvent {
+    data class AddItem(val item: InventoryItem) : InventoryEvent()
+    data class Refresh(val isRefreshing: Boolean) : InventoryEvent()
+}
